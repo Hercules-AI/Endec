0dec3c4f720d5c27027e114d75b453c873aaf500
heliyahasani <heliyahasani@hotmail.com>
2024-05-21 13:29:04 +0300

    logs added

--- app/endpoints/endec_decoder.py
+++ app/endpoints/endec_decoder.py
@@ -13,7 +13,7 @@ router = APIRouter()
 
 
 @router.post("/fbis/decoder")
-async def create_query(request: Request,compressed_file_path: str = Body(...)):
+async def create_query(request: Request,compressed_file_path: str = Body(..., embed=True)):
     service = CreateDecodedFileService()
     created_query = await service.execute(compressed_file_path)
     return created_query
